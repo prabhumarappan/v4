2aef7eefec84e1ce0b2f2d11ac219cd7e970fca8
Prabhu Marappan <prabhum.794@gmail.com>
2021-08-12 23:28:35 +0530

    Updated few parts of the code

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Prabhu Marappan',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Prabhu Marappan is experienced software engineer with around 5 years of work experience in backend and infra.',
+    siteUrl: 'https://prabhumarappan.me', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@prabhu794',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'PrabhuMarappan',
+        short_name: 'PrabhuMarappan',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
